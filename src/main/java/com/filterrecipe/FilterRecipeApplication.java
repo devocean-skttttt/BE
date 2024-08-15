36f197f09c95b3e3e92d257affb9438b6c30f743
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-08-15 23:25:42 +0900

    [init] 프로젝트 초기화

---
+++ src/main/java/com/filterrecipe/FilterRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.filterrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterRecipeApplication.class, args);
+    }
+
+}
