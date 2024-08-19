879188f399b8cb62a6fdce7230405eefd48133aa
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-08-19 19:38:25 +0900

    [fix] JwtToken userId 기준으로 생성

--- src/main/java/com/filterrecipe/auth/repository/UserRepository.java
+++ src/main/java/com/filterrecipe/auth/repository/UserRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> getUserByLoginId(String loginId);
-
     Boolean existsByLoginId(String loginId);
 }
