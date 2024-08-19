a204d966103fa2052226d17f3e2f51003a0d1ef5
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-08-19 20:22:19 +0900

    [feat] Recipe와 Tag Repository 생성

---
+++ src/main/java/com/filterrecipe/recipe/repository/RecipeRepository.java
@@ -0,0 +1,12 @@
+package com.filterrecipe.recipe.repository;
+
+import com.filterrecipe.recipe.dto.Recipe;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Optional<Set<Recipe>> findByTagsTagId(Long tagId);
+}
