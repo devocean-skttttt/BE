13bd4ae141f00df5b019fd620dc714a80e4229c6
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-08-26 21:15:05 +0900

    [feat] Bookmark 엔터티 추가

---
+++ src/main/java/com/filterrecipe/recipe/dto/Bookmark.java
@@ -0,0 +1,26 @@
+package com.filterrecipe.recipe.dto;
+
+import com.filterrecipe.auth.dto.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "bookmark")
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmarkId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+}
